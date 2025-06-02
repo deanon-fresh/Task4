903c3bfc309e16a7ea10903e79dbf50fe2f8edfd
deanon-fresh <deanon.fresh@yandex.ru>
2025-06-02 17:11:22 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-public static int year3 = 0; // для сравнения
+public static int daysUsers = 0; // для сравнения
 public static int attempt = 0; // для счетчика
 
-    public static void calculation(int year) {
+    public static int calculation(int year) {
         if (year % 400 == 0) {
             //System.out.println("Количество дней 366");
-            year3 = 366;
+            daysUsers = 366;
         } else if (year % 4 == 0 && year % 100 != 0) {
             //System.out.println("Количество дней 366");
-            year3 = 366;
+            daysUsers = 366;
         } else  {
             //System.out.println("Количество дней 365");
-            year3 = 365;
+            daysUsers = 365;
         }
+    return daysUsers;
     }
 
 
@@ -24,16 +25,16 @@ public static int attempt = 0; // для счетчика
             System.out.print("Введите год в формате гггг: ");
             int year = scanner.nextInt();
             System.out.print("Введите количество дней в указанном Вами году: ");
-            int year2 = scanner.nextInt();
-            calculation(year);
-            if (year2 == year3) {
+            int days = scanner.nextInt();
+            int daysUsers = calculation(year);
+            if (days == daysUsers) {
                 //System.out.println("Верно, продолжаем");
                 attempt++;
             } else {
                 break;
                 }
             }
-        System.out.println("Неправильно! В этом году " + year3 + " дней!");
+        System.out.println("Неправильно! В этом году " + daysUsers + " дней!");
         System.out.println("Набрано очков: " + attempt);
         }
 
